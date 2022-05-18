e933f3a87cd6d1983f0fb92a73124753ce72ade0
kwakdonghyun <kwakdonghyun@gmail.com>
2022-05-18 22:12:21 +0900

    [검색폼2]중간정리

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,6 +1,82 @@
-// main.js만든이유
-// index.html에서 다하면 코드너무많아짐
-// main.js시작점으로할것이고
+class App extends React.Component {
+    constructor() {
+        super();
 
-const element = <h1>Hello World</h1>
-ReactDOM.render(element, document.querySelector("#app"));
+        this.state = {
+            searchKeyword: "",
+        }
+    }
+
+    handleChangeInput(event) {
+        // this.state.searchKeyword = event.target.value
+        // this.forceUpdate();
+        const searchKeyword = event.target.value
+        if (searchKeyword.length <= 0) {
+            return this.handleReset()
+        }
+        this.setState({
+            searchKeyword: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault() // 화면리프레시 막는다.
+        console.log('handleSubmit', this.state.searchKeyword)
+    }
+
+    handleReset() {
+        console.log('handleReset')
+        this.setState(
+            () => {
+                return { searchKeyword: "" }
+            },
+            () => {
+
+            })
+    }
+    // render 오버라이딩
+    render() { // react Element를 반환
+
+        // let resetButton = null;
+        // if (this.state.searchKeyword.length > 0) {
+        //     resetButton = <button type="reset" className="btn-reset"></button>
+        // }
+
+        return (
+            <>
+                <header>
+                    <h2 className="container">검색</h2>
+                </header>
+                <div className="container">
+                    <form
+                        onSubmit={event => this.handleSubmit(event)}
+                        onReset={() => this.handleReset()}
+                    >
+                        <input
+                            type="text"
+                            placeholder="검색어를 입력하세요"
+                            autoFocus
+                            name='searchForm'
+                            value={this.state.searchKeyword}
+                            // onChange={this.handleChange}
+                            onChange={event => this.handleChangeInput(event)}
+                        />
+
+                        {this.state.searchKeyword.length > 0 && (
+                            <button
+                                type="reset"
+                                className="btn-reset"
+                            >
+
+                            </button>
+                        )}
+                    </form>
+                </div>
+            </>
+        )
+    }
+}
+
+// 컴포넌넌트화 해줘야한다. <App/> 써주면 App은 react Element를 반환하게된다.
+// ReactDom이 가상돔을 그리고 실제돔에게 반환
+ReactDOM.render(<App />, document.querySelector("#app"));
